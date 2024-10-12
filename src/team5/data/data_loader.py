68dbe98a063481e43f8ca0aa95d0d86213b4a620
Paco Cordobes <ghiret@gmail.com>
2024-10-13 00:25:06 +0100

    Example code using the data split and the tokenizer

--- src/team5/data/data_loader.py
+++ src/team5/data/data_loader.py
@@ -1,69 +1,19 @@
-# import the data (with pandas?)
-import pandas as pd
+import polars as pl
+from data_split import sort_dataframe_by_scaffold, split_dataframe
+from prepare import tensorize
 
-## Load the dataset (for some reason this didn't work for me)
-# df = pd.read_parquet('enveda_library_subset 2.parquet')
+if __name__ == "__main__":
+    print(f"Loading the data!.")
 
-# print(df.head())
+    parquet_file = "data/raw/enveda_library_subset_10percent.parquet"
+    df = pl.read_parquet(parquet_file)
 
+    df_sorted = sort_dataframe_by_scaffold(df)
 
-# tokenize the SMILES. Do we need to pad? If so, what's the max length
-def tokenize_function(examples):
-    return tokenizer(
-        examples["smiles"], truncation=True, padding="max_length", max_length=128
-    )
+    df_train, df_test = split_dataframe(df_sorted, 0.9)
 
+    (tokenized_smiles, labels, supplementary_data) = tensorize(df_train, len(df_train))
 
-# custom Dataset class for all the types of data.
-# I think we might want to make a new 'column' of data that combines mzs and intensities into "label"
-
-from torch.utils.data import Dataset
-import torch
-
-
-class SMILESDataset(Dataset):
-    def __init__(self, dataframe, tokenizer, max_seq_length):
-        self.tokenizer = tokenizer
-        self.max_seq_length = max_seq_length
-        self.smiles = dataframe["smiles"].tolist()
-        self.precursor_mz = dataframe["precursor_mz"].tolist()
-        self.precursor_charge = dataframe["precursor_charge"].tolist()
-        self.collision_energy = dataframe["collision_energy"].tolist()
-        self.instrument_type = dataframe["instrument_type"].tolist()
-        self.in_silico_label = dataframe["in_silico"].tolist()
-        self.adduct = dataframe["adduct"].tolist()
-        self.compound_class = dataframe["compound_class"].tolist()
-        self.mzs = dataframe["mzs"].tolist()
-        self.intensities = dataframe["intensities"].tolist()
-
-        # Create labels as a 2D array of mzs and intensities put together. Or have it flat and just concat both
-        # self.labels = #TODO
-
-        # Create supplementary data as a long concatinated list of all the supplementary data
-        # self.supplementary_data = #TODO
-
-    def __len__(self):
-        return len(self.smiles)
-
-    def __getitem__(self, idx):
-        smiles = self.smiles[idx]
-        precursor_mz = self.precursor_mz[idx]
-        label = self.labels[idx]
-
-        # Tokenize SMILES
-        inputs = self.tokenizer(
-            smiles,
-            truncation=True,
-            padding="max_length",
-            max_length=self.max_seq_length,
-            return_tensors="pt",
-        )
-
-        # Prepare item
-        item = {
-            key: val.squeeze(0) for key, val in inputs.items()
-        }  # Remove batch dimension
-        item["precursor_mz"] = torch.tensor(precursor_mz, dtype=torch.float)
-        item["labels"] = torch.tensor(label, dtype=torch.long)
-
-        return item
+    print("tokenized_smiles: ", tokenized_smiles)
+    print("labels: ", labels)
+    print("supplementary_data: ", supplementary_data)
