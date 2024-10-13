4fa4752b77205747c000a85dbf2e14ca333d13c5
Paco Cordobes <ghiret@gmail.com>
2024-10-13 09:24:16 +0100

    Example code using the data split and the tokenizer (#8)

--- src/team5/data/data_loader.py
+++ src/team5/data/data_loader.py
@@ -1,53 +1,19 @@
-# import the data (with pandas?)
-import pandas as pd
+import polars as pl
+from data_split import sort_dataframe_by_scaffold, split_dataframe
+from prepare import tensorize
 
-## Load the dataset (for some reason this didn't work for me)
-# df = pd.read_parquet('enveda_library_subset 2.parquet')
+if __name__ == "__main__":
+    print(f"Loading the data!.")
 
-# print(df.head())
+    parquet_file = "data/raw/enveda_library_subset_10percent.parquet"
+    df = pl.read_parquet(parquet_file)
 
+    df_sorted = sort_dataframe_by_scaffold(df)
 
-# tokenize the SMILES. Do we need to pad? If so, what's the max length
-def tokenize_function(examples):
-    return tokenizer(
-        examples["smiles"], truncation=True, padding="max_length", max_length=128
-    )
+    df_train, df_test = split_dataframe(df_sorted, 0.9)
 
+    (tokenized_smiles, labels, supplementary_data) = tensorize(df_train, len(df_train))
 
-# custom Dataset class for all the types of data.
-# I think we might want to make a new 'column' of data that combines mzs and intensities into "label"
-
-from torch.utils.data import Dataset
-import torch
-
-
-class SMILESDataset(Dataset):
-    def __init__(self, tokenized_smiles, attention_mask, labels, supplementary_data):
-        self.tokenized_smiles = tokenized_smiles
-        self.attention_mask = attention_mask
-        self.labels = labels
-        self.supplementary_data = supplementary_data
-
-        # Create labels as a 2D array of mzs and intensities put together. Or have it flat and just concat both
-        # self.labels = #TODO
-
-        # Create supplementary data as a long concatinated list of all the supplementary data
-        # self.supplementary_data = #TODO
-
-    def __len__(self):
-        return len(self.tokenized_smiles)
-
-    def __getitem__(self, idx):
-        tokenized_smiles = self.tokenized_smiles[idx]
-        attention_mask = self.attention_mask[idx]
-        label = self.labels[idx]
-        supplementary_data = self.supplementary_data[idx]
-
-        return {
-            "input_ids": tokenized_smiles,
-            "attention_mask": attention_mask,
-            "labels": label,
-            "supplementary_data": supplementary_data
-        }
-    
-   
+    print("tokenized_smiles: ", tokenized_smiles)
+    print("labels: ", labels)
+    print("supplementary_data: ", supplementary_data)
