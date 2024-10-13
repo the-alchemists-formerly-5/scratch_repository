2d316782eb7ff4849123d42fb9a1ec690fa65e91
Lars Mennen <mennen.lars@gmail.com>
2024-10-13 11:22:01 +0100

    Make notebook run with dummy loss

--- src/team5/data/data_loader.py
+++ src/team5/data/data_loader.py
@@ -1,6 +1,32 @@
 import polars as pl
-from data_split import sort_dataframe_by_scaffold, split_dataframe
-from prepare import tensorize
+from src.team5.data.data_split import sort_dataframe_by_scaffold, split_dataframe
+from src.team5.data.prepare import tensorize
+from torch.utils.data import Dataset
+
+
+class SMILESDataset(Dataset):
+
+    def __init__(self, tokenized_smiles, attention_mask, labels, supplementary_data):
+        self.tokenized_smiles = tokenized_smiles
+        self.attention_mask = attention_mask
+        self.labels = labels
+        self.supplementary_data = supplementary_data
+
+    def __len__(self):
+        return len(self.tokenized_smiles)
+    
+    def __getitem__(self, idx):
+        tokenized_smiles = self.tokenized_smiles[idx]
+        attention_mask = self.attention_mask[idx]
+        label = self.labels[idx]
+        supplementary_data = self.supplementary_data[idx]
+
+        return {
+            "input_ids": tokenized_smiles,
+            "attention_mask": attention_mask,
+            "labels": label,
+            "supplementary_data": supplementary_data
+        }
 
 if __name__ == "__main__":
     print(f"Loading the data!.")
